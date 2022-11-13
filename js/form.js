506ef0b32c77e338f21b6f2a3df180bc4ef4b86b
Kuza_Ko <cveta-mavk@mail.ru>
2022-11-13 19:36:04 +0800

    добавляет пояснения

--- js/form.js
+++ js/form.js
@@ -2,7 +2,7 @@ const roomsToGuests = {
   1: ['1'],
   2: ['1', '2'],
   3: ['1', '2', '3'],
-  100: ['2'],
+  100: ['0'],
 };
 
 const guestsToRooms = {
@@ -37,13 +37,16 @@ const pristine = new Pristine(addFormElement, {
   errorTextClass: 'ad-form__element--invalid',
 }, true);
 
+//нашли нужные формы
 const amountRoomNumber = addFormElement.querySelector('#room_number');
 const amountCapacity = addFormElement.querySelector('#capacity');
 
+//функция в массиве комнат проверяет есть ли в указанном ключе(кол-ве комнат) нужное число гостей
 function validateCapacity() {
   return roomsToGuests[amountRoomNumber.value].includes(amountCapacity.value);
 }
 
+//сообжение об ошибке
 function amountCapacityErrorMessage() {
   return `Указанное количество комнат вмещает ${roomsToGuests[amountRoomNumber.value].join (' или ')} гостей.`;
 }
@@ -52,6 +55,7 @@ function validateCapacityErrorMessage() {
   return `Для указанного количества гостей требуется ${guestsToRooms[amountCapacity.value].join (' или ')} комнаты.`;
 }
 
+//фун-я изменения значений
 function onCapacityChange() {
   pristine.validate(amountCapacity);
   pristine.validate(amountRoomNumber);
@@ -62,12 +66,15 @@ function onRoomNumberChange() {
   pristine.validate(amountRoomNumber);
 }
 
+//добавляет пристин(элемент,фунция валидации,сообщение об ошибке)
 pristine.addValidator(amountRoomNumber, validateCapacity, validateCapacityErrorMessage);
 pristine.addValidator(amountCapacity, validateCapacity, amountCapacityErrorMessage);
 
+//обработчики событий на случай если значение поменяется
 amountRoomNumber.addEventListener('change', onCapacityChange);
 amountCapacity.addEventListener('change', onRoomNumberChange);
 
+//подключаем пристин к форме
 addFormElement.addEventListener('submit', (evt) => {
   evt.preventDefault();
   pristine.validate();
